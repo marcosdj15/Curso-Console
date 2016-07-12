7a13c196b8eb9097b79e3bcfec1a740e3150f352
marcosdj15 <marcosdj2140@gmail.com>
2016-07-11 21:59:12 -0300

    Commit Codigo Pronto

--- CursoConsole/CursoConsole/Program.cs
+++ CursoConsole/CursoConsole/Program.cs
@@ -10,6 +10,72 @@ namespace CursoConsole
     {
         static void Main(string[] args)
         {
+            int opcao, operacao;
+            string cliente,produto;
+            Console.Clear();
+            Console.WriteLine("1-Cliente");
+            Console.WriteLine("2-Produto");
+            Console.WriteLine("3-Sair");
+            opcao = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcao)
+            {
+                
+                case 1:
+                    Console.WriteLine("1-Cadastrar Cliente");
+                    Console.WriteLine("2-Consultar Cliente");
+                    break;
+                    
+                case 2:
+                    Console.WriteLine("1-Cadastrar Produto");
+                    Console.WriteLine("2-consultar Produto");
+                    break;
+                case 3:
+                    Console.WriteLine("Saindo.....");
+                    break;       
+            default :
+                    Console.WriteLine("Operação Inválida");
+                    break;
+
+                    if ((opcao.Equals(1)))
+                    {
+                        operacao = Convert.ToInt32(Console.ReadLine());
+                        if ((operacao.Equals(1)))
+	                     {
+                            Console.WriteLine("Informe o Nome");
+                            cliente = Console.ReadLine();
+	                     }
+                        else if ((operacao.Equals(2)))
+	                    {
+		                   Console.WriteLine("Nome do Cliente"+cliente);
+                            Console.ReadKey();
+	                    }
+                        else
+                        {
+                            Console.WriteLine("Opção Inválida");
+                        }
+                           
+                    }
+                    else if ((opcao.Equals(2)))
+	                {
+		               operacao = Convert.ToInt32(Console.ReadLine());
+                        if ((operacao.Equals(1)))
+	                    {
+		                   Console.WriteLine("Digite o Nome do Produto");
+                            produto = Console.ReadLine();
+	                    }
+                        else if((operacao.Equals(2)))
+                        {
+                            Console.WriteLine("Nome do Produto"+produto);
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Opção Inválida");
+                        }
+                           
+	                }
+            }
         }
     }
 }
